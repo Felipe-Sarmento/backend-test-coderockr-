7aa0a570c50b13fd5589ef0ec668607f4d219045
Felipe Sarmento <felipe.sarmento@ucsbr.com>
2024-02-03 18:23:54 -0300

    feat: create investment service

---
+++ src/investment/services/investment.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { InvestmentEntity } from '../dto/investment.model';
+import { GetInvestmentUsecase } from '../usecases/get-investment.usecase';
+import { CreateInvestmentUsecase } from '../usecases/create-investment.usecase';
+import { UpdateInvestmentUsecase } from '../usecases/update-investment.usecase';
+import { DeleteInvestmentUsecase } from '../usecases/delete-investment.usecase';
+
+@Injectable()
+export class InvestmentService {
+  constructor(
+    private readonly getInvestmentUseCase: GetInvestmentUsecase,
+    private readonly createInvestmentUseCase: CreateInvestmentUsecase,
+    private readonly updateInvestmentUseCase: UpdateInvestmentUsecase,
+    private readonly deleteInvestmentUseCase: DeleteInvestmentUsecase,
+  ) {}
+
+  async createInvestment(
+    amount: number,
+    ownerId: string,
+  ): Promise<InvestmentEntity> {
+    return this.createInvestmentUseCase.execute(amount, ownerId);
+  }
+
+  async viewInvestment(id: string): Promise<InvestmentEntity> {
+    return this.getInvestmentUseCase.execute(id);
+  }
+
+  async deleteInvestment(id: string): Promise<string> {
+    return this.deleteInvestmentUseCase.execute(id);
+  }
+
+  async updateInvestment(
+    id: string,
+    amount: number,
+  ): Promise<InvestmentEntity> {
+    return this.updateInvestmentUseCase.execute(id, amount);
+  }
+}
