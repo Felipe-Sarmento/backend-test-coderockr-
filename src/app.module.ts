ad7b737fe9100bfff46c09e7c57cd902122d98b1
Felipe Sarmento <felipe.sarmento@ucsbr.com>
2024-02-03 18:24:48 -0300

    feat: removing unused basic files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { InvestimentModule } from './investiment/investiment.module';
+import { InvestmentModule } from './investment/investment.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [InvestimentModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    InvestmentModule,
+    MongooseModule.forRoot('mongodb://localhost:27017'),
+  ],
 })
 export class AppModule {}
