3ffc340b933c1e5349c03824930fc72f4522aca7
Felipe Sarmento <felipe.sarmento@ucsbr.com>
2024-02-03 18:25:37 -0300

    feat: adding exception filter and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { HttpExceptionFilterHandler } from './shared/filter/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Investment API')
+    .setDescription('The investment API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.useGlobalFilters(new HttpExceptionFilterHandler());
+
   await app.listen(3000);
 }
 bootstrap();
